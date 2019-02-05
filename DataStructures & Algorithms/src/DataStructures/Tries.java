bb35e2254f3420096c2e771a394bc1099d2aab3a
sreekanth_concord <sreekanths.dev@auxolabs.in>
2019-02-06 01:01:49 +0530

    Updated with another approach

--- DataStructures & Algorithms/src/DataStructures/Tries.java
+++ DataStructures & Algorithms/src/DataStructures/Tries.java
@@ -4,6 +4,54 @@ import java.util.LinkedList;
 
 public class Tries {
 	
+public static class Node {
+		
+		private static int NUMBER_OF_CHARACTERS=26;
+		Node children[]=new Node[NUMBER_OF_CHARACTERS];
+		int size=0;
+		
+		private static int getCharIndex(char c) {
+			return c-'a';
+		}
+		
+		private Node getNode(char c) {
+			return children[getCharIndex(c)];
+		}
+		
+		private void setNode(char c,Node node) {
+			children[getCharIndex(c)]=node;
+		}
+		
+		public void add(String s) {
+			add(s,0);
+		}
+		
+		public void add(String s, int index) {
+			if(index==s.length())
+				return;
+			char current=s.charAt(index);
+			int charCode=getCharIndex(current);
+			Node child=getNode(current);
+			if(child==null) {
+				child=new Node();
+				setNode(current,child);
+			}
+			child.add(s,index+1);
+		}
+		
+		public int findCount(String s, int index) {
+			if(index==s.length()) {
+				return size;
+			}
+			Node child=getNode(s.charAt(index));
+			if(child==null) {
+				return 0;
+			}
+			return child.findCount(s, index+1);
+		}
+	}
+	
 	public static void main(String[] args) {
+		
 	}
 }
