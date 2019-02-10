a2094bd883705d2e33d52370a61182a085312ac0
srees16 <s.srees@live.com>
2019-02-10 23:36:14 +0530

    Minor corrections

--- DataStructures & Algorithms/src/DataStructures/StackInt.java
+++ DataStructures & Algorithms/src/DataStructures/StackInt.java
@@ -7,30 +7,30 @@ public class StackInt {
 	private int size;
 	
 	private StackInt() {
-		top=-1;
-		size=1;
-		stack=new int[size];
+		top =- 1;
+		size = 1;
+		stack = new int[size];
 	}
 	
 	private StackInt(int size) {
-		top=-1;
-		this.size=size;
-		stack=new int[this.size];
+		top =- 1;
+		this.size = size;
+		stack = new int[this.size];
 	}
 	
 	private void reSize() {
 		if(isFull()) {
-			int temp[]=stack;
-			size*=2;
-			stack=new int[size];
-			for(int i=0;i<=top;i++) {
-				stack[i]=temp[i];
+			int temp[] = stack;
+			size *= 2;
+			stack = new int[size];
+			for(int i = 0; i <= top; i++) {
+				stack[i] = temp[i];
 			}
 		}
 	}
 
 	private boolean isFull() {
-		return (top==stack.length-1);
+		return (top == stack.length-1);
 	}
 	
 	public void peek() {
@@ -38,24 +38,24 @@ public class StackInt {
 	}
 	
 	private boolean isEmpty() {
-		return (top==-1);
+		return (top == -1);
 	}
 	
 	public int push(int item) {
 		if(!isFull()) {
 			top++;
-			stack[top]=item;
+			stack[top] = item;
 		} else {
 			reSize();
 			top++;
-			stack[top]=item;
+			stack[top] = item;
 		}
 		return item;
 	}
 	
 	public Integer pop() {
 		if(!isEmpty()) {
-			int item=stack[top];
+			int item = stack[top];
 			top--;
 			return item;
 		} else {
@@ -64,19 +64,19 @@ public class StackInt {
 	}
 	
 	public void printAll() {
-		int f=top;
+		int item = top;
 		if(!isEmpty()) {
-			for(int i=0;i<=top;i++) {
-				System.out.print(stack[f]+" ");
-				f-=1;
+			for(int i = 0; i <= top; i++) {
+				System.out.print(stack[item] + " ");
+				item -= 1;
 			}
 		}
 	}
 	
 	public boolean search(int item) {
 		if(!isEmpty()) {
-			for(int i=0;i<=top;i++) {
-				if(stack[i]==item) {
+			for(int i = 0; i <= top; i++) {
+				if(stack[i] == item) {
 					return true;
 				}
 			}
@@ -85,15 +85,15 @@ public class StackInt {
 	}
 	
 	public static void main(String[] args) {
-		StackInt si=new StackInt(2);
-		si.push(56);
-		si.push(12);
-		si.push(26);
-		si.push(85);
-		si.printAll();
-		System.out.println(si.search(45));
-		si.pop();
-		si.printAll();
-		si.peek();
+		StackInt stack = new StackInt(2);
+		stack.push(56);
+		stack.push(12);
+		stack.push(26);
+		stack.push(85);
+		stack.printAll();
+		System.out.println(stack.search(45));
+		stack.pop();
+		stack.printAll();
+		stack.peek();
 	}
 }
