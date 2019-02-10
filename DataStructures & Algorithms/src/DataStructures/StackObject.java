d7eba2e133548d8cd5f53470ee00c207e135f1a8
srees16 <s.srees@live.com>
2019-02-10 23:25:47 +0530

    Search method reworked

--- DataStructures & Algorithms/src/DataStructures/StackObject.java
+++ DataStructures & Algorithms/src/DataStructures/StackObject.java
@@ -1,30 +1,30 @@
 package DataStructures;
 
-public class StackObject { //Rework needed to search using name
+public class StackObject {
 
 	private int top;
 	private Employee stack[];
 	private int size;
 	
 	private StackObject() {
-		top=-1;
-		size=1;
-		stack=new Employee[size];
+		top = -1;
+		size = 1;
+		stack = new Employee[size];
 	}
 	
 	private StackObject(int size) {
-		top=1;
-		this.size=size;
-		stack=new Employee[this.size];
+		top = 1;
+		this.size = size;
+		stack = new Employee[this.size];
 	}
 	
 	private void reSize() {
 		if(isFull()) {
-			Employee temp[]=stack;
-			size*=2;
-			stack=new Employee[size];
-			for(int i=0;i<=top;i++) {
-				stack[i]=temp[i];
+			Employee temp[] = stack;
+			size *= 2;
+			stack = new Employee[size];
+			for(int i = 0; i <= top; i++) {
+				stack[i] = temp[i];
 			}
 		}
 	}
@@ -32,18 +32,18 @@ public class StackObject { //Rework needed to search using name
 	public Employee push(Employee item) {
 		if(!isFull()) {
 			top++;
-			stack[top]=item;
+			stack[top] = item;
 		} else {
 			reSize();
 			top++;
-			stack[top]=item;
+			stack[top] = item;
 		}
 		return item;
 	}
 	
 	public void pop() {
 		if(!isEmpty()) {
-			Employee item=stack[top];
+			Employee item = stack[top];
 			top--;
 		} else {
 			throw new IllegalStateException("No elements to pop");
@@ -55,19 +55,19 @@ public class StackObject { //Rework needed to search using name
 	}
 	
 	public void print() {
-		int x=top;
+		int x = top;
 		if(!isEmpty()) {
-			for(int i=0;i<=top;i++) {
-				System.out.println(stack[x]+" ");
-				x-=1;
+			for(int i = 0; i <= top; i++) {
+				System.out.println(stack[x] + " ");
+				x -= 1;
 			}
 		}
 	}
 	
-	public boolean search(Employee name) {
+	public boolean search(String name) {
 		if(!isEmpty()) {
-			for(int i=0;i<=top;i++) {
-				if(stack[i]==name) {
+			for(int i = 0; i <= top; i++) {
+				if(name.equals(stack[i].empName.toString())) {
 					return true;
 				}
 			}
@@ -76,18 +76,18 @@ public class StackObject { //Rework needed to search using name
 	}
 	
 	private boolean isEmpty() {
-		return (size==-1);
+		return (size == -1);
 	}
 	
 	private boolean isFull() {
-		return (top==stack.length-1);
+		return (top == stack.length-1);
 	}
 	
 	public static void main(String[] args) {
-		Employee e1=new Employee("Adam",415554,23);
-		Employee e2=new Employee("Bob",432554,13);
-		Employee e3=new Employee("Charlie",485354,81);
-		StackObject so=new StackObject();
+		Employee e1 = new Employee("Adam", 415554, 23);
+		Employee e2 = new Employee("Bob", 432554, 13);
+		Employee e3 = new Employee("Charlie", 485354, 81);
+		StackObject so = new StackObject();
 		so.push(e1);
 		so.push(e2);
 		so.push(e3);
@@ -96,5 +96,6 @@ public class StackObject { //Rework needed to search using name
 		System.out.println("====");
 		so.pop();
 		so.print();
+		System.out.println(so.search("Bob"));
 	}
 }
