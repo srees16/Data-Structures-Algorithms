9fb0bca609b6aa9fa239a6d6b949af598e53c00b
sreekanth_concord <sreekanths.dev@auxolabs.in>
2019-02-11 12:15:41 +0530

    Updated QueueInt class

--- DataStructures & Algorithms/src/DataStructures/QueueInt.java
+++ DataStructures & Algorithms/src/DataStructures/QueueInt.java
@@ -8,64 +8,64 @@ public class QueueInt {
 	private int queue[];
 	
 	public QueueInt() {
-		front=0;
-		rear=0;
-		size=1;
-		queue=new int[size];
+		front = 0;
+		rear = 0;
+		size = 1;
+		queue = new int[size];
 	}
 	
 	public QueueInt(int size) {
-		front=0;
-		rear=0;
-		this.size=size;
-		queue=new int[this.size];
+		front = 0;
+		rear = 0;
+		this.size = size;
+		queue = new int[this.size];
 	}
 	
 	private void reSize() {
-		int temp[]=queue;
-		size*=2;
-		queue=new int[size];
-		for(int i=0;i<=rear;i++) {
-			queue[i]=temp[i];
+		int temp[] = queue;
+		size *= 2;
+		queue = new int[size];
+		for(int i = 0; i <= rear; i++) {
+			queue[i] = temp[i];
 		}
 	}
 	
-	public int enq(int item) {
+	public int enqueue(int item) {
 		if(!isFull()) {
-			queue[rear]=item;
-			rear=(rear+1)%size;
+			queue[rear] = item;
+			rear = (rear + 1) % size;
 		} else {
 			reSize();
-			queue[rear]=item;
-			rear=(rear+1)%size;
+			queue[rear] = item;
+			rear = (rear + 1) % size;
 		}
 		return item;
 	}
 	
-	public void deq() {
+	public void dequeue() {
 		if(!isEmpty()) {
-			int item=queue[front];
-			front=(front+1)%size;
+			int item = queue[front];
+			front = (front + 1) % size;
 		} else {
 			throw new IllegalStateException("You cant delete more man!");
 		}
 	}
 	
 	public void print() {
-		int x=front;
+		int x = front;
 		if(!isEmpty()) {
-			for(int i=0;i<rear;i++) {
+			for(int i = 0; i < rear; i++) {
 				System.out.println(queue[x]);
-				x+=1;
+				x += 1;
 			}
 		}
 	}
 	
 	public boolean search(int item) {
-		int f=front;
-		if(f!=-1) {
-			for(int i=0;i<=rear;i++) {
-				if(queue[i]==item) {
+		int f = front;
+		if(!isEmpty()) {
+			for(int i = 0; i <= rear; i++) {
+				if(queue[i] == item) {
 					return true;
 				}
 			}
@@ -74,25 +74,25 @@ public class QueueInt {
 	}
 	
 	private boolean isFull() {
-		return size==queue.length;
+		return (size == queue.length);
 	}
 	
 	private boolean isEmpty() {
-		return front==rear;
+		return (front == rear);
 	}
 	
 	public static void main(String[] args) {
-		QueueInt q=new QueueInt();
-		q.enq(41);
-		q.enq(15);
-		q.enq(52);
-		q.enq(72);
-		q.enq(48);
-		q.enq(62);
-		q.print();
-		q.deq();
-		System.out.println();
-		q.print();
-		System.out.println(q.search(752));
+		QueueInt queue = new QueueInt();
+		queue.enqueue(41);
+		queue.enqueue(15);
+		queue.enqueue(52);
+		queue.enqueue(72);
+		queue.enqueue(48);
+		queue.enqueue(62);
+		queue.print();
+		queue.dequeue();
+		System.out.println("====");
+		queue.print();
+		System.out.println(queue.search(752));
 	}
 }
