68deaa531f79973af4b19ed5453cfc5ebc27f3c4
sreekanth_concord <sreekanths.dev@auxolabs.in>
2019-02-11 23:03:13 +0530

    Minor updation

--- DataStructures & Algorithms/src/DataStructures/LinkedListInt.java
+++ DataStructures & Algorithms/src/DataStructures/LinkedListInt.java
@@ -10,48 +10,48 @@ public class LinkedListInt {
 	}
 	
 	public LinkedListInt() {
-		head=new Node();
-		head.data=0;
-		head.next=null;
+		head = new Node();
+		head.data = 0;
+		head.next = null;
 	}
 	
 	public boolean insertPre(int item) {
-		Node n=new Node();
-		n.data=item;
-		n.next=head;		
-		head=n;
+		Node node = new Node();
+		node.data = item;
+		node.next = head;		
+		head = node;
 		return true;
 	}
 	
 	public boolean insertPost(int item) {
-		Node n=new Node();
-		Node x=head;
-		while(x.next!=null) {
-			x=x.next;
+		Node node = new Node();
+		Node x = head;
+		while(x.next != null) {
+			x = x.next;
 		}
-		n.data=item;
-		n.next=null;
-		x.next=n;
+		node.data = item;
+		node.next = null;
+		x.next = node;
 		return true;
 	}
 	
 	public void delete(int item) {
 		if(isPresent(item)) {
-			Node x=head;
-			if(x.data==item) {
-				head=head.next;
+			Node x = head;
+			if(x.data == item) {
+				head = head.next;
 			} else {
-				Node y=head.next;
+				Node y = head.next;
 				while(true) {
-					if(y==null || y.data==item) {
+					if(y == null || y.data == item) {
 						break;
 					} else {
-						x=y;
-						y=y.next;
+						x = y;
+						y = y.next;
 					}
 				}
-				if(y.data==item) {
-					x.next=y.next;
+				if(y.data == item) {
+					x.next = y.next;
 				}
 			}
 		} else {
@@ -60,46 +60,46 @@ public class LinkedListInt {
 	}
 	
 	private boolean isPresent(int item) {
-		Node x=head.next;
-		while(x!=null) {
-			if(x.data==item) {
+		Node x = head.next;
+		while(x != null) {
+			if(x.data == item) {
 				return true;
 			}
-			x=x.next;
+			x = x.next;
 		}
 		return false;
 	}
 	
 	public void print() {
-		Node x=head.next;
-		while(x!=null) {
-			System.out.println(x.data+" ");
-			x=x.next;
+		Node x = head.next;
+		while(x != null) {
+			System.out.println(x.data + " ");
+			x = x.next;
 		}
 	}
 	
 	public static void main(String[] args) {
-		LinkedListInt ll = new LinkedListInt();
-		/*ll.insertPre(56);
-		ll.insertPre(22);
-		ll.insertPre(68);
-		ll.insertPre(35);
-		ll.insertPre(73);
-		ll.insertPre(15);
-		ll.print();*/
+		LinkedListInt linkedList = new LinkedListInt();
+		/*linkedList.insertPre(56);
+		linkedList.insertPre(22);
+		linkedList.insertPre(68);
+		linkedList.insertPre(35);
+		linkedList.insertPre(73);
+		linkedList.insertPre(15);
+		linkedList.print();*/
 		
-		ll.insertPost(56);
-		ll.insertPost(22);
-		ll.insertPost(68);
-		ll.insertPost(35);
-		ll.insertPost(73);
-		ll.insertPost(15);
-		ll.print();
+		linkedList.insertPost(56);
+		linkedList.insertPost(22);
+		linkedList.insertPost(68);
+		linkedList.insertPost(35);
+		linkedList.insertPost(73);
+		linkedList.insertPost(15);
+		linkedList.print();
 		System.out.println();
-		ll.delete(22);
-		ll.print();
+		linkedList.delete(22);
+		linkedList.print();
 		System.out.println();
-		System.out.println(ll.isPresent(56));
-		ll.delete(65);
+		System.out.println(linkedList.isPresent(56));
+		linkedList.delete(65);
 	}
 }
